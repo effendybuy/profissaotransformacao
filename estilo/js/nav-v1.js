d1a4ec0dab0b4f116369bb1b8b6f915883d03472
Ewerton Leonardo <effendybuy@outlook.com>
2021-07-07 23:03:16 -0300

    Design v1

--- estilo/js/nav-v1.js
+++ estilo/js/nav-v1.js
@@ -3,8 +3,9 @@ window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
   if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-    document.getElementsByClassName("nav")[0].style.backgroundColor = "black";
+    document.getElementsByClassName("nav")[0].style.backgroundColor = "white";
     document.getElementsByClassName("nav")[0].style.display = "flex";
+    document.getElementsByClassName("nav")[0].style.color = "black";
   } else {
     document.getElementsByClassName("nav")[0].style.backgroundColor = "transparent";
     document.getElementsByClassName("nav")[0].style.display = "none";
